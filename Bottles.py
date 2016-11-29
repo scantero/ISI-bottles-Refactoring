f40a3f81350eb40eec00f6964b72fc489b834fd1
scantero <sm.cantero@gmail.com>
2016-11-29 13:27:55 +0100

    Posible solución sin if en método verse()

--- Bottles.py
+++ Bottles.py
@@ -9,22 +9,36 @@ class Bottles:
             o += "\n" + self.verse (i)
         return o
 
+    def number_to_string(self, number):
+        if number == 0:
+            return "no more"
+        elif number == -1:
+            return str(99)
+        else:
+            return str(number)
+
     def container(self, number):
         if number == 1:
             return "bottle"
         else:
             return "bottles"
 
-    def verse(self, number = None):
+    def begin_2nd_line(self, number):
         if number == 0:
-            return """No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""
+            return "Go to the store and buy some more, "
         elif number == 1:
-            return """1 bottle of beer on the wall, 1 bottle of beer.
-Take it down and pass it around, no more bottles of beer on the wall.
-"""
+            return "Take it down and pass it around, "
         else:
-            return """{0} {2} of beer on the wall, {0} {2} of beer.
-Take one down and pass it around, {1} {3} of beer on the wall.
-""".format(number, number-1, self.container(number), self.container(number-1))
+            return "Take one down and pass it around, "
+
+    def upper_first_letter(self, cadena):
+        letra = cadena[0]
+        cadena = cadena[1:]
+        letra = letra.upper()
+        return letra + cadena
+
+    def verse(self, number = None):
+        return "{0} {1} of beer on the wall, ".format(self.upper_first_letter(self.number_to_string(number)), self.container(number)) +\
+        "{0} {1} of beer.\n".format(self.number_to_string(number), self.container(number)) +\
+        self.begin_2nd_line(number) +\
+        "{0} {1} of beer on the wall.\n".format(self.number_to_string(number-1), self.container(number-1))
