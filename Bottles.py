5e5447fc58b7334df858b3d725cff59e88372ae8
scantero <sm.cantero@gmail.com>
2016-11-30 12:13:21 +0100

    Añadida clase ContainerNumber con error que no consigo arreglar

--- Bottles.py
+++ Bottles.py
@@ -9,24 +9,40 @@ class Bottles:
             o += "\n" + self.verse (i)
         return o
 
-    def units_left(self, number):
-        if number == 0:
+    def verse(self, number=None):
+        c = ContainerNumber(number)
+        c_ant = ContainerNumber(number-1)
+        return "{0} of beer on the wall, ".format(c.upper_first_letter(c.set_of_drinks()) +\
+        "{0} of beer.\n".format(c.set_of_drinks()) +\
+        c.next_action() +\
+        "{0} of beer on the wall.\n".format(c_ant.set_of_drinks())
+
+class ContainerNumber:
+
+    def __init__(self, containers):
+        self.containers = containers
+
+    def set_of_drinks(self):
+        return "{0} {1}".format(self.units_left(self.containers), c.container(self.containers))
+
+    def units_left(self):
+        if self.containers == 0:
             return "no more"
-        elif number == -1:
+        elif self.containers == -1:
             return str(99)
         else:
-            return str(number)
+            return str(self.containers)
 
-    def container(self, number):
-        if number == 1:
+    def container(self):
+        if self.containers == 1:
             return "bottle"
         else:
             return "bottles"
 
-    def next_action(self, number):
-        if number == 0:
+    def next_action(self):
+        if self.containers == 0:
             return "Go to the store and buy some more, "
-        elif number == 1:
+        elif self.containers == 1:
             return "Take it down and pass it around, "
         else:
             return "Take one down and pass it around, "
@@ -36,9 +52,3 @@ class Bottles:
         cadena = cadena[1:]
         letra = letra.upper()
         return letra + cadena
-
-    def verse(self, number = None):
-        return "{0} {1} of beer on the wall, ".format(self.upper_first_letter(self.units_left(number)), self.container(number)) +\
-        "{0} {1} of beer.\n".format(self.units_left(number), self.container(number)) +\
-        self.next_action(number) +\
-        "{0} {1} of beer on the wall.\n".format(self.units_left(number-1), self.container(number-1))
